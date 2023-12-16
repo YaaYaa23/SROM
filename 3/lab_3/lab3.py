d13be37a84ac37c26c128a6da1457044f3849722
yaya2 <yarfed-ipt25@lll.kpi.ua>
2023-12-16 18:04:09 +0200

    “mulldiv”

--- 3/lab_3/lab3.py
+++ 3/lab_3/lab3.py
@@ -26,3 +26,78 @@ def add_pol(A, B):
     A, B = equal_length(A, B)
     result = [(a + b) % 2 for a, b in zip(A, B)]
     return mod_pol(result)
+
+def mull_pol(A, B):
+    if A == 0 or B == 0:
+        return 0
+    A, B = equal_length(A, B)
+    result = [0] * (2 * len(A))
+    for i, a in enumerate(A):
+        if a == 0:
+            continue
+        for j, b in enumerate(B):
+            if b == 0:
+                continue
+            result[i + j] = (result[i + j] + a * b) % 2
+    return mod_pol(result)
+
+def mod_pol(number):
+    ourMod = [0] * 192
+    ourMod[191], ourMod[9], ourMod[0] = 1, 1, 1
+    _, remainder = div_pol(number, ourMod)
+    return remainder
+
+def div_pol(A, B):
+    A = pop_null(A)
+    compare = cmp(A, B)
+    if compare == 0:
+        return [0], [0]
+    if compare == -1:
+        return [0], A
+
+    k = len(B)
+    r = A.copy()
+    q = [0] * len(A)
+
+    while cmp(r, B) != -1:
+        t = len(r)
+        c = append_null(B, t - k)
+        r = sub_pol(r, c)
+        q[t - k] = 1
+
+    q = pop_null(q)
+    r = pop_null(r)
+    return q, r
+
+def cmp(A, B):
+    A = pop_null(A)
+    B = pop_null(B)
+
+    if A != 0:
+        lenA = len(A)
+    else:
+        lenA = 0
+
+    if B != 0:
+        lenB = len(B)
+    else:
+        lenB = 0
+
+    if lenA > lenB:
+        return 1
+    elif lenA < lenB:
+        return -1
+    else:
+        for i in range(len(A) - 1, -1, -1):
+            if A[i] == B[i]:
+                pass
+            elif A[i] > B[i]:
+                return 1
+            else:
+                return -1
+
+    return 0
+
+def append_null(array, k):
+    return [0] * k + array
+
